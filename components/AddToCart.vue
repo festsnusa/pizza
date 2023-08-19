872756588ec1521dcc24486572bd0dfd14a8bcea
festsnusa <lukatimof@mail.ru>
2023-08-19 13:06:01 +0700

    feat: new component

---
+++ components/AddToCart.vue
@@ -0,0 +1,22 @@
+<script setup>
+const arr = [
+  { "title": "Картофель из печи", "price": "179", "img": "_nuxt/assets/images/potato.svg", "additional": "Порция 95 г" },
+  { "title": "Сырный соус", "price": "179", "img": "_nuxt/assets/images/potato.svg" }
+]
+</script>
+
+<template>
+  <div class="container">
+    <div class="cards">
+      <div class="card" v-for="item in arr">
+        <img :src="item.img">
+        <span>{{ item.title }}</span>
+        <span v-if="item.additional">{{ item.additional }}</span>
+        <button>{{ item.price }} ₽</button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
