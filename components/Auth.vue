631d235d29b7221b45e996f186ccbcf506a80d3b
festsnusa <lukatimof@mail.ru>
2023-08-24 20:11:54 +0700

    test: new component

---
+++ components/Auth.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hi there</h1>
+    <img src="../assets/images/icon-close.svg" alt="close">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
