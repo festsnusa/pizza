800cd6408d4f13ff6c8189de75c50dbc838a907b
festsnusa <lukatimof@mail.ru>
2023-08-26 17:02:24 +0700

    feat: new component ItemCard

---
+++ components/ItemCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <NewLabel v-if="label" />
+    <img src="" alt="pizzaItem">
+    <div class="caption">
+      <p class="title">Пепперони по-деревенски</p>
+      <p class="subtitle">Огурцы маринованные,</p>
+    </div>
+    <div class="footer">
+      <Button text="Выбрать" />
+      <span class="price">от 399 ₽</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: Boolean,
+    default: false
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  // position: absolute;
+}
+
+.price {
+  color: #FF7010;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 24px;
+}
+</style>
---
+++ components/NewLabel.vue
@@ -0,0 +1,18 @@
+<template>
+  <label for="">New</label>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+label {
+  // position: absolute;
+  text-transform: uppercase;
+  background-color: #E23535;
+  color: #fff;
+  border-radius: 0 10px 10px 0;
+  padding: 1rem 1.5rem;
+}
+</style>
