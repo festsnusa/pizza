e7bc05a63527d656f7df0c5621410cbfd0a9bca7
festsnusa <lukatimof@mail.ru>
2023-08-31 18:27:00 +0700

    feat: dropdown component

---
+++ components/Dropdown.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-dropdown>
+    <a class="ant-dropdown-link" @click.prevent>
+      Другое
+      <DownOutlined />
+    </a>
+    <template #overlay>
+      <a-menu>
+        <a-menu-item v-for="(option, i) in options" :key="`option__${i}`">
+          <NuxtLink :to="option.link">
+            {{ option.title }}
+          </NuxtLink>
+        </a-menu-item>
+      </a-menu>
+    </template>
+  </a-dropdown>
+</template>
+
+<script setup>
+import { DownOutlined } from '@ant-design/icons-vue'
+
+const options = [
+  { "title": "Акции", "link": "actions" },
+  { "title": "О компании", "link": "about" },
+  { "title": "Пользовательское соглашение", "link": "terms-of-use" },
+  { "title": "Условия гарантии", "link": "warranty-terms" },
+  { "title": "Ресторан", "link": "restaurant" },
+  { "title": "Контакты", "link": "contacts" },
+  { "title": "Поддержка", "link": "support" },
+  { "title": "Отследить заказ", "link": "track-order" },
+]
+</script>
+
+<style lang="scss" scoped>
+.ant-dropdown-link {
+  cursor: pointer;
+}
+</style>
