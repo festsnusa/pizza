f7eec135f52060088c2902a12600a0f83ebba8c5
festsnusa <lukatimof@mail.ru>
2023-08-31 18:28:39 +0700

    feat: added toggleFilterMenu

--- components/Button/ButtonFilter.vue
+++ components/Button/ButtonFilter.vue
@@ -1,12 +1,12 @@
 <template>
-  <Button class="white">
+  <Button class="white" @click="toggleFilterMenu(true)">
     <img src="_nuxt/assets/images/filter.svg" alt="filter">
     <span>Фильтры</span>
   </Button>
 </template>
 
 <script setup>
-
+const { toggleFilterMenu } = useFilter()
 </script>
 
 <style lang="scss" scoped></style>
