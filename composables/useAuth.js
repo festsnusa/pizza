f9c51dc7b8a8b1d3d606cf25b90c9cbb705a454a
festsnusa <lukatimof@mail.ru>
2023-09-02 00:03:02 +0700

    feat: no-scroll for modal

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -3,6 +3,12 @@ const useAuth = () => {
 
   const toggleModal = () => {
     isModalVisible.value = !isModalVisible.value;
+
+    if (isModalVisible.value) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
   };
 
   return {
